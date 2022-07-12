c5c3f50317e85bf487ad1de81ae91ca915b5fe4a
saniyar-dev <saniyar.dev@gmail.com>
2022-07-12 14:20:38 +0430

    end of Ui, start of solution in js

--- index.js
+++ index.js
@@ -3,29 +3,47 @@ const restartButton = document.getElementById('restart_button');
 const app = document.getElementById('app');
 const nInputField = document.getElementById('n');
 
+
+const solve = () => {
+    console.log('solved');
+};
+
 const template = `<div class="flex justify-center flex-col gap-6">
+            <h1></h1>
             <div>
                 <input type="text" id="name" placeholder="Enter a name" class="p-3 border-2 focus:border-blue-600">
             </div>
             <div>
-                <input type="text" id="name" placeholder="Enter a food name" class="p-3 border-2 focus:border-blue-600">
+                <input type="text" id="food" placeholder="Enter a food name" class="p-3 border-2 focus:border-blue-600">
             </div>
             <div>
-                <input type="text" id="name" placeholder="Enter a color name" class="p-3 border-2 focus:border-blue-600">
+                <input type="text" id="color" placeholder="Enter a color name" class="p-3 border-2 focus:border-blue-600">
             </div>
             <div>
-                <input type="text" id="name" placeholder="Enter a city name" class="p-3 border-2 focus:border-blue-600">
+                <input type="text" id="city" placeholder="Enter a city name" class="p-3 border-2 focus:border-blue-600">
             </div>
         </div>`;
 
 const render = (n) => {
-    console.log(n)
+    for (let i = 0; i < n; i++) {
+        const el = document.createElement('div');
+        el.innerHTML = template;
+        const h1 = el.querySelector('h1');
+        h1.innerText = `Participant ${i +1}:`
+        app.appendChild(el)
+    }
+    const el = document.createElement('div');
+    el.innerHTML = `        <button id="finish_button" class="bg-slate-700 text-white p-5 rounded-lg">
+            Finish 
+        </button>`;
+    el.addEventListener('click', () => {
+        solve();
+    });
+    app.appendChild(el);
 };
 
 startButton.addEventListener('click', () => {
     render(nInputField.value);
-
-    startButton.removeEventListener('click');
 });
 
 restartButton.addEventListener('click', () => {
